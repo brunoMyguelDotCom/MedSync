c9c733628ad019fcef1d8a48a87c063547165170
Nicholas Catelan Goes <eunicodev@gmail.com>
2026-05-05 20:24:39 -0300

    Fix EspecialidadeRepository remocao import desnecessario

--- src/main/java/com/example/demo/repository/EspecialidadeRepository.java
+++ src/main/java/com/example/demo/repository/EspecialidadeRepository.java
@@ -1,13 +1,8 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.Entities.Especialidade;
 
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long>{
-
-
     
 } 
