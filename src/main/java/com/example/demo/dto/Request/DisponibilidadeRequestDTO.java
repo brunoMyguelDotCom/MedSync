9deedcba518b2892870ed7e388995e50c769bca6
Nicholas Catelan Goes <eunicodev@gmail.com>
2026-05-05 19:58:17 -0300

    fix: tipo de atributo corrigido

--- src/main/java/com/example/demo/dto/Request/DisponibilidadeRequestDTO.java
+++ src/main/java/com/example/demo/dto/Request/DisponibilidadeRequestDTO.java
@@ -1,15 +1,14 @@
 package com.example.demo.dto.Request;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
-
-import com.example.demo.Entities.Enums.DiaSemana;
 
 import jakarta.validation.constraints.NotNull;
 
 public record DisponibilidadeRequestDTO(
 
     @NotNull(message = "Dia da semana é obrigatório")
-    DiaSemana diaSemana,
+    DayOfWeek diaSemana,
 
     @NotNull(message = "Horário de início é obrigatório")
     LocalTime horarioInicio,
--- src/main/java/com/example/demo/repository/DisponibilidadeRepository.java
+++ src/main/java/com/example/demo/repository/DisponibilidadeRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.Entities.Disponibilidade;
 import com.example.demo.Entities.Medico;
-import com.example.demo.Entities.Enums.DiaSemana;
 
 public interface DisponibilidadeRepository extends JpaRepository<Disponibilidade, Long> {
 
@@ -14,5 +14,5 @@ public interface DisponibilidadeRepository extends JpaRepository<Disponibilidade
     List<Disponibilidade> findByMedico(Medico medico);
 
     // buscar por dia da semana
-    List<Disponibilidade> findByMedicoAndDiaSemana(Medico medico, DiaSemana diaSemana);
+    List<Disponibilidade> findByMedicoAndDiaSemana(Medico medico, DayOfWeek diaSemana);
 }
