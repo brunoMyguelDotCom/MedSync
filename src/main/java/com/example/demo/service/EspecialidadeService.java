562ca4415467ec6d952c4c23f51839f03dfbb315
Guilherme Paiva <guipfriedrich@gmail.com>
2026-05-19 19:22:09 -0300

    HotFix: ImportServiceEspecialidade

--- src/main/java/com/example/demo/service/EspecialidadeService.java
+++ src/main/java/com/example/demo/service/EspecialidadeService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.Entities.Especialidade;
 import com.example.demo.dto.Request.EspecialidadeRequestDTO;
 import com.example.demo.dto.Response.EspecialidadeResponseDTO;
